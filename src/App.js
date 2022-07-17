54df864fbb8a842a4ffd3bcbc4c43900a7b889be
tingamapuro04 <alponcemobutu100@gmail.com>
2022-07-17 03:52:26 +0300

    Complete

--- src/App.js
+++ src/App.js
@@ -2,34 +2,13 @@ import './App.css';
 import Card from './components/Card';
 import Grid from './components/Grid';
 import Nav from './components/Nav';
-import nina from './img/nina.jpg'
-import junior from './img/junior.jpg'
-import nicole from './img/nicole.jpg'
+import List from './components/elements';
 
-const info = [
-  {
-    id: 1,
-    src: nina,
-    name: 'Ninnah',
-    age: 7
-  },
-  {
-    id: 2,
-    src: nicole,
-    name: 'Junior',
-    age: 5
-  },
-  {
-    id: 3,
-    src: junior,
-    name: 'Nicole',
-    age: 12
-  }
-]
+
 
 function App() {
-  const madocho = info.map(item => {
-    return <Card src={item.src} name={item.name} age={item.age} key={item.id} />
+  const madocho = List.map(item => {
+    return <Card src={item.img} name={item.title} age={item.stats.rating} key={item.id}  openSpots={item.openSpots} location={item.location}/>
   })
   return (
     <div className="App">
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,19 @@
 import React from 'react'
 
 
-function Card({src, name, age}) {
+function Card({src, name, age, openSpots, location}) {
+  let badgeText;
+  if (openSpots === 0) {
+    badgeText= "SOLD OUT"
+  } else if (location === "Online") {
+    badgeText = location
+  }
   return (
     <div className="card">
+      {badgeText && <div className="sold"> {badgeText} </div>}
       <img src={src} alt="pic" className="nina-pic" />
       {name && <h2> {name} </h2>}
-      <p> {age} years old </p>
+      <p> {age} stars <span>{location}</span></p>
     </div>
   )
 }
